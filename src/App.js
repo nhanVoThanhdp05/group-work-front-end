1177a668e76cf94c4d59a23d528295fd2fa9cd1a
nhanVoThanhdp05 <nhan.vo_dp05@devplus.edu.vn>
2023-03-27 13:24:21 +0700

    dev: dpm-install-essential-packages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, DatePicker } from 'antd';
+import 'antd/dist/antd.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Button type="primary">PRESS ME</Button>
+      <DatePicker placeholder="select date" />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <div class="alert alert-primary" role="alert">
+      <p className="header">Welcome to our application</p>
+    </div>
+  );
+}
